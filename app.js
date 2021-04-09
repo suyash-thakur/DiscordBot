7f743b8f43501526454e546205bdde5faf0aa9d0
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-04-10 00:33:23 +0530

    communicating with nodejs

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ require("dotenv").config();
 const http = require('http').Server(app);
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const threadCount = +process.argv[2] || 2;
+const threads = new Set();
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+var querystring = require('querystring');
+const baseURL = 'https://www.google.com/search?q=';
 client.login(process.env.BOT_TOKEN);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,34 +24,100 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 if (isMainThread) {
-    const max = 1e7;
-    const threadCount = +process.argv[2] || 2;
-    const threads = new Set();;
-    console.log(`Running with ${threadCount} threads...`);
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
+  const max = 1e7;
+
+  console.log(`Running with ${threadCount} threads...`);
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
     for (let i = 0; i < threadCount - 1; i++) {
-        threads.add(new Worker('./worker.js', { workerData: { message: 'zebra' }}));
+  }
+  for (let worker of threads) {
+      worker.on('error', (err) => { throw err; });
+      worker.on('exit', () => {
+        threads.delete(worker);
+        console.log(`Thread exiting, ${threads.size} running...`);
+        if (threads.size === 0) {
+          console.log("No Process Running");
+        }
+      })
+      worker.on('message', (msg) => {
+        console.log(chalk.cyanBright.bold(msg));
+  
+        client.channels.cache.get("827619928235442259").send(msg.slice(0, 50));
+      });
     }
-    for (let worker of threads) {
-        worker.on('error', (err) => { throw err; });
-        worker.on('exit', () => {
-          threads.delete(worker);
-          console.log(`Thread exiting, ${threads.size} running...`);
-          if (threads.size === 0) {
-            console.log("No Process Running");
-          }
-        })
-        worker.on('message', (msg) => {
-            console.log(chalk.cyanBright.bold(msg));
-        });
-      }
+  });
+  
 }
 client.on('message', msg => {
     if (msg.content === 'ping' && msg.channel.id === '827619928235442259') {
       msg.reply('pong');
-    }
+  }
+  if (msg.content.split(" ")[0] == '!search') {
+    threads.add(new Worker('./worker.js', { workerData: { message: msg.content.split(" ")[1] }}));
+    msg.reply('searching...');
+
+  }
+  if (msg.content.split(" ")[0] == '!google') {
+    var url = baseURL + msg.content.split(" ")[1];
+    var linkSel = 'h3.r a'
+var descSel = 'div.s'
+var itemSel = 'div.g'
+var nextSel = 'td.b a span'
+    rp(url).then(function (html) {
+
+      var $ = cheerio.load(html)
+      var res = {
+        links: [],
+        $: $,
+      }
+
+      $(itemSel).each(function (i, elem) {
+        var linkElem = $(elem).find(linkSel)
+        var descElem = $(elem).find(descSel)
+        var item = {
+          title: $(linkElem).first().text(),
+          link: null,
+          description: null,
+          href: null
+        }
+        var qsObj = querystring.parse($(linkElem).attr('href'))
+
+        if (qsObj['/url?q']) {
+          item.link = qsObj['/url?q']
+          item.href = item.link
+        }
+
+        $(descElem).find('div').remove()
+        item.description = $(descElem).text()
+
+        res.links.push(item);
+        console.log(res);
+      });
+      console.log(res);
+
+    }).catch(function (err) {
+      console.log(err);
+    msg.reply('Error Finding Search Result');
+
+  });
+  }
+  for (let worker of threads) {
+    worker.on('error', (err) => { throw err; });
+    worker.on('exit', () => {
+      threads.delete(worker);
+      console.log(`Thread exiting, ${threads.size} running...`);
+      if (threads.size === 0) {
+        console.log("No Process Running");
+      }
+    })
+    worker.on('message', (msg) => {
+      console.log(chalk.cyanBright.bold(msg));
+      var sendMsg = msg;
+
+      client.channels.cache.get("827619928235442259").send(sendMsg.slice(0, 1000) + "...");
+    });
+  }
 });
 
 http.listen(process.env.PORT || 3000, function () {
